06b1641f1ccd8d2abdd2170a56640f7cf509d28a
Dominik Liebler <liebler.dominik@gmail.com>
2021-10-02 13:23:02 +0200

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,59 @@
+const mix = require('laravel-mix')
+
+/**
+ * By default, AdonisJS public path for static assets is on the `./public` directory.
+ *
+ * If you want to change Laravel Mix public path, change the AdonisJS public path config first!
+ * See: https://docs.adonisjs.com/guides/static-assets#the-default-directory
+ */
+mix.setPublicPath('public')
+
+// Add your assets here
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const webpack = require('webpack')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+mix
+  .setPublicPath('public')
+  .js('resources/client/index.js', 'public/js/')
+  .react()
+  .sass('resources/assets/scss/index.scss', 'public/css/')
+  .options({
+    processCssUrls: false,
+  })
+if (isDevelopment) {
+  mix.sourceMaps()
+}
+mix.webpackConfig({
+  mode: isDevelopment ? 'development' : 'production',
+  context: __dirname,
+  node: {
+    __filename: true,
+    __dirname: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              presets: ['@babel/preset-react'],
+              plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+  ].filter(Boolean),
+})
