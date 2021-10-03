e27c24b324064fc59ed637c3408a334e8419acea
Dominik Liebler <liebler.dominik@gmail.com>
2021-10-03 12:59:30 +0200

    Amount: show currency (fixed to Euro)

--- resources/client/components/Amount.tsx
+++ resources/client/components/Amount.tsx
@@ -10,5 +10,5 @@ export default function Amount(props: AmountProps) {
   const size = props.size ?? 'small';
   const color = props.amount > 0 ? 'success' : 'error';
 
-  return <Chip label={props.amount.toFixed(2)} color={color} size={size} />;
+  return <Chip label={props.amount.toFixed(2) + ' €'} color={color} size={size} />;
 }
