dd396679123004f279176ce6d4b0226d6cfe05be
Dominik Liebler <liebler.dominik@gmail.com>
2021-10-04 11:42:10 +0200

    fixed formatDate month is 0-based

--- resources/client/Utils.ts
+++ resources/client/Utils.ts
@@ -1,6 +1,6 @@
 export function formatDate(date: Date) {
-  let month = date.getMonth().toString().padStart(2, '0');
-  let day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
 
   return date.getFullYear() + '-' + month + '-' + day;
 }
