7f234af17ea700bf9476eb4280f049d00027e7cc
Dominik Liebler <liebler.dominik@gmail.com>
2021-10-05 08:05:51 +0200

    add burger menu and drawer for main navigation

--- resources/client/App.tsx
+++ resources/client/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Dashboard from './components/Dashboard';
 import Box from '@mui/material/Box';
@@ -6,7 +6,12 @@ import {
   AppBar,
   Container,
   createTheme,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   ThemeProvider,
   Toolbar,
   Typography,
@@ -16,9 +21,11 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { LocalizationProvider } from '@mui/lab';
 import enLocale from 'date-fns/locale/en-GB';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
   const theme = React.useMemo(
@@ -46,11 +53,23 @@ export default function App() {
                   color="inherit"
                   aria-label="menu"
                   sx={{ mr: 2 }}
+                  onClick={() => setDrawerOpen(!drawerOpen)}
                 >
                   <MenuIcon />
+
+                  <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+                    <List>
+                      <ListItem button>
+                        <ListItemIcon>
+                          <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" />
+                      </ListItem>
+                    </List>
+                  </Drawer>
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  <Link to="/">bookkeeping</Link>
+                  bookkeeping
                 </Typography>
               </Toolbar>
             </AppBar>
