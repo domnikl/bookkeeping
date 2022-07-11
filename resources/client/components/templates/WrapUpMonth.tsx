f7a7460561059d348086b39a7a840d556ee44bd3
Hanni <johanna.liebler@gmail.com>
2022-07-11 14:21:01 +0200

    #13 fix Wrapup date

--- resources/client/components/templates/WrapUpMonth.tsx
+++ resources/client/components/templates/WrapUpMonth.tsx
@@ -17,7 +17,7 @@ export default function WrapUpMonth(props: WrapUpMonthProps) {
   const dueDates = props.categories
     .map((x) => x.dueDate)
     .filter((x) => x != null)
-    .sort((a: Date, b: Date) => b.getDate() - a.getDate());
+    .sort((a: Date, b: Date) => (a.getDate() > b.getDate() ? 1 : -1));
 
   const smallest = dueDates[0] ?? new Date();
 
