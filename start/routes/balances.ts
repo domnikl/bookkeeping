df3495b8c318ed49d72cd0cdb2e5f2b162342286
Dominik Liebler <dominik.liebler@yazio.com>
2022-08-12 09:06:09 +0200

    aggregate data for the BalanceGraph in the backend

--- start/routes/balances.ts
+++ start/routes/balances.ts
@@ -1,16 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route';
-import BalanceModel from 'App/Models/BalanceModel';
+import { findBalancesByMonth } from 'Database/balances';
 
 Route.get('balances/:iban/:from/:to', async ({ request }) => {
   const from = request.params().from;
   const to = request.params().to;
   const iban = request.params().iban;
 
-  return await BalanceModel.query()
-    .whereBetween('bookingDate', [from, to])
-    .whereHas('account', (accountQuery) => {
-      accountQuery.where('iban', iban);
-    })
-    .orderBy('bookingDate', 'desc')
-    .preload('account');
+  return await findBalancesByMonth(iban, from, to);
 });
